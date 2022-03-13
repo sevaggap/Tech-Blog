088a6bc15b6009c39b2c13007ab5e085e2a7c8d6
sevaggap <sevag.gaprielian@gmail.com>
2022-03-12 20:55:36 -0500

    wrote script in homeRoutes to get usernames based on comment user_id, and aggregate a comment object which included the comments content, creation date, user id and user name, streamlined handlebar styling

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -54,19 +54,45 @@ router.get('/post/:id', async (req,res) => {
 
             const post = postData.get({ plain: true });
 
-            const comment_user_id = post.comments[0].user_id;
-            const usercommentData = await User.findByPk(comment_user_id, { attributes:['username'] });
-
-            const usercomment = usercommentData.get({ plain: true });
-
-            console.log(comment_user_id);
-            console.log(usercomment);
             console.log(post);
 
-            res.render('post', {
-                ...post, usercomment, logged_in: req.session.logged_in
-            });
+            const user_ids = [];
+            const user_names = [];
+            const comments = [];
 
+            for(i=0;i<post.comments.length;i++){
+                let user_id = post.comments[i].user_id;
+                user_ids.push(user_id);
+            }
+
+            console.log(user_ids);
+
+            for(i=0;i<user_ids.length;i++){
+                const usercommentData = await User.findByPk(user_ids[i], { attributes:['username'] });
+
+                const user_name = usercommentData.get({ plain: true });
+
+                user_names.push(user_name);
+            }
+
+            for(i=0;i<postData.comments.length;i++) {
+                const comment = {
+                    content: postData.comments[i].content,
+                    date_created: postData.comments[i].date_created,
+                    user_id: postData.comments[i].user_id,
+                    user_name: user_names[i].username,
+                }
+
+                comments.push(comment);
+            }
+            
+            console.log(comments);
+
+            console.log(user_names);
+
+            res.render('post', {
+                ...post, comments, logged_in: req.session.logged_in
+            });
         } else {
             console.log(post);
             res.render('post', {
@@ -77,8 +103,6 @@ router.get('/post/:id', async (req,res) => {
         res.status(500).json(err);
     }
 });
-
-
 
 router.get('/dashboard', withAuth, async (req,res) => {
     try{
